33bcaadbf325a6f4ece9a70c8b22dc869028a193
Kaven Feng <kaven.feng@autodesk.com>
2018-07-30 15:59:08 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by fengka on 7/30/18.
+//  Copyright © 2018 fengka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
